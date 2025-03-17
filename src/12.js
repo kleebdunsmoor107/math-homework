fb890f11a4a7963e2b940172955388e388ca6cc8
kleebdunsmoor107 <kleebdunsmoor107@gmail.com>
2025-03-17 05:55:13 +0000

    Add 12.js

---
+++ src/12.js
@@ -0,0 +1,8 @@
+// Function to solve math homework problems
+function solveMathHomework(problem) {
+  // Implement your solution here
+}
+
+// Example usage of the function
+console.log(solveMathHomework("2 + 3")); // Output: 5
+console.log(solveMathHomework("4 - 2")); // Output: 2
