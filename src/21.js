d0ca1c4a888b4172b34f31ca111c58bebbe1c9bd
kleebdunsmoor107 <kleebdunsmoor107@gmail.com>
2025-03-30 10:11:55 +0000

    Add 21.js

---
+++ src/21.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) throw new Error('Error: Division by zero is not allowed');
+  return a / b;
+}
